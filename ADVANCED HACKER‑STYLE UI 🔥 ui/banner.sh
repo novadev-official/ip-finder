d98475e5f8ad3b7ecc58a6d7a1d79c5e96ca80e8
NOVA DEVELOPMENTS <novadevelopmentoffical@gmail.com>
2025-12-15 21:19:41 +0530

    Add menu options to banner script

--- ADVANCED HACKER‑STYLE UI 🔥 ui/banner.sh
+++ ADVANCED HACKER‑STYLE UI 🔥 ui/banner.sh
@@ -12,3 +12,10 @@ EOF
 echo -e "\e[0m"
 echo "        ⚡ NOVA BEST UI ⚡"
 echo
+
+ui/menu.sh
+#!/bin/bash
+echo -e "\e[1;32m[1]\e[0m System Info"
+echo -e "\e[1;32m[2]\e[0m Network Info"
+echo -e "\e[1;32m[3]\e[0m Exit"
+read -p "NOVA > " choice
